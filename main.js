79b11f6aea440dedeefeb9f8efd1a93c877e3cb8
Cedric <cedric.jamme@edu.devinci.fr>
2022-01-30 20:33:39 +0100

    start second api

--- main.js
+++ main.js
@@ -30,6 +30,12 @@ var requestOptions = {
 	  }
 	})
 	
+let sectionGot = document.getElementById('section-got')
+
+sectionGot.style.display = 'none'
+
+let logoGot = document.getElementById('logo-got').style.display ='none'
+
 const containeur = document.querySelector(".containeur");
 document.body.style.backgroundImage ="url('./asset/bg-api.jpg')";
 document.body.style.backgroundSize ='cover';
@@ -45,6 +51,22 @@ function RenderHogwartsCharacters(chara) {
 		</div>
 
 		<style>
+
+		.contain{
+			display:flex;
+			flex-direction:column;
+			align-items:center;
+			gap:9px;
+			cursor:pointer
+		}
+
+		h3{
+			font-family:'Harry P';
+			color:rgb(201,141,40);
+			font-size:1.4rem;
+			letter-spacing:3px;
+			text-align:center;
+		}
 		
 		img{
 			width:100px;
@@ -104,7 +126,8 @@ let triChara = result.sort();
 }
 
 var selectChara =document.getElementsByClassName('select-chara')[0];
-
+selectChara.style.display='flex';
+selectChara.style.justifyContent='center';
 
 function ShowChara(wizard){
 
@@ -113,9 +136,9 @@ function ShowChara(wizard){
 	selectChara.style.display ='flex';
 
 	selectChara.innerHTML = `
-	<div class='contain' onclick="SelectCharacter('${wizard["name"]}')">
-		  <h3 id='color-name'>${wizard["name"]}</h3>
-		  <img src="${wizard["image"]}">
+	<div class='contain contain-select' onclick="SelectCharacter('${wizard["name"]}')">
+		  <h3 id='color-name' class='title-select'>${wizard["name"]}</h3>
+		  <img id='img-select' src="${wizard["image"]}">
 		  <p id='color-actor'>${wizard["actor"]}</p>
 		  <p>${wizard["gender"]}</p>
 		  <p>${wizard["ancestry"]}</p>
@@ -124,6 +147,30 @@ function ShowChara(wizard){
 		  <p>${wizard["house"]}</p>
 		  <p>${wizard["dateOfBirth"]}</p>
 	</div>
+
+	<style scoped>
+	
+	#img-select{
+		height:300px;
+		width:200px
+	}
+	
+	p{
+		color:white;
+		text-align:center;
+		font-weight : bold;
+	}
+	
+	.title-select{
+		font-size:2.5rem
+	}
+
+	.contain-select{
+		background-color : rgb(201,141,40, 0.7);
+		border-radius :5px;
+		padding: 20px;
+	}
+	</style>
 	`
 }
 
@@ -168,7 +215,6 @@ function SelectByHouse(house){
 }
 
 
-
 selectHouse.addEventListener('change', ()=>{
 	SelectByHouse(selectHouse.value)
 })
@@ -184,6 +230,81 @@ selectHouse.addEventListener('change', ()=>{
 
 
 
+function SelectCharacterGot(name){
+
+	fetch('https://thronesapi.com/api/v2/Characters')
+	.then((response) => response.json())
+	.then((result)=>{
+
+		let CharactersGot = new Array(20)
+		for (let i = 0; i < 20; i++) {
+			CharactersGot[i] = result[i]
+			console.log(result)
+		}
+
+	})
+
+}
+var showcharaGot =document.getElementsByClassName('showcharaGot')[0];
+function ShowCharaGot(charactersgot){
+
+
+	showcharaGot.innerHTML = `
+	<div class='contain contain-select'>
+		  <h3 id='color-name' class='title-select'>${charactersgot["fullName"]}</h3>
+		  <img id='img-select' src="${charactersgot["imageUrl"]}">
+		  <p>${charactersgot["firstName"]}</p>
+		  <p>${charactersgot["LastName"]}</p>
+		  <p>${charactersgot["title"]}</p>
+		  <p>${charactersgot["family"]}</p>
+	</div>`
+	
+}
+function SearchGotCharacters(name){
+
+}
+
+
+const buttonChange = document.getElementById('switch')
+
+function Change(){
+	let state='bg1';
+
+	buttonChange.addEventListener('click', ()=>{
+		if(state == 'bg1'){
+			document.body.style.backgroundImage = "url('/asset/The-Wall.jpg')"
+			state='bg2';
+
+			document.getElementById('logo-got').style.display ='flex'
+
+			document.getElementById('logo-potter').style.display ='none'
+
+			containeur.style.display='none'
+
+			selectHouse.style.display='none'
+
+			sectionGot.style.display = 'flex'
+
+			selectChara.style.display='none';
+			
+		}else{
+			document.body.style.backgroundImage = "url('/asset/bg-api.jpg')"
+			state='bg1';
+			
+			document.getElementById('logo-potter').style.display ='flex'
+			document.getElementById('logo-got').style.display ='none'
+
+			containeur.style.display='flex'
+
+			selectHouse.style.display='flex'
+
+			 sectionGot.style.display ='none'
+		}
+	})
+
+}
+
+Change()
 
 
 
